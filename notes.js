8bfc6c5468de23669f09fb96559e93481f44d138
marclou <marc.louvion@gmail.com>
2018-03-14 16:10:02 +0900

    refactoring addNote

--- notes.js
+++ notes.js
@@ -1,6 +1,35 @@
+const fs = require('fs');
+
+const fetchNote = () => {
+  try {
+    let notesExistingString = fs.readFileSync('notes.json');
+    return JSON.parse(notesExistingString);
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
+
+const saveNotes = (notes) => {
+
+};
+
 module.exports = {
   addNote: (title, body) => {
-    console.log(`Adding note : ${title}, ${body}`);
+    let notes = fetchNote();
+    let note = {
+      title,
+      body
+    }
+
+    let duplicateNotes = notes.filter(note => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+      notes.push(note);
+      fs.writeFileSync('notes.json', JSON.stringify(notes));
+    } else {
+      console.log('Duplicated title ...')
+    }
   },
   getAll: () => {
     console.log('Getting all notes');
@@ -11,4 +40,4 @@ module.exports = {
   removeNote: (title) => {
     console.log(`Remove note ${title}`);
   }
-}
+};
